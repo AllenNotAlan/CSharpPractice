cc09ca0fece715097702e93cbf2c5f0387ce7aac
AllenNotAlan <allenloyola23@gmail.com>
2020-07-23 14:54:52 +0100

    checkPrimality solution added

---
+++ CheckPrimality_11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheckPrimality_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+
+            Console.WriteLine("Enter a number to see if it's a prime or not: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(test.isPrime(userNum));
+        }
+
+        public bool isPrime(int num)
+        {
+            bool isPrime = false;
+            List<int> divisors = new List<int>();
+
+            for(int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+
+            if ((divisors.Count <= 2) && (divisors[0] == 1))
+            {
+                isPrime = true;
+                Console.WriteLine("{0} is a prime", num);
+            }
+            return isPrime;
+        }
+    }
+}
---
+++ ListComprehension/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListComprehension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArr1 = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+            int[] testArr2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+            Program testSolution = new Program();
+            //testSolution.result(testArr1, testArr2);
+
+            printResults(testSolution.result(testArr1, testArr2));
+        }
+
+        public HashSet<int> result(int[] arr1, int[] arr2)
+        {
+            int arr1Length = arr1.Length;
+            int arr2Length = arr2.Length;
+
+            HashSet<int> results = new HashSet<int>();
+
+            for (int i = 0; i < arr1Length; i++)
+            {
+                for (int j = 0; j < arr2Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {
+                        results.Add(arr1[i]);
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public static void printResults(HashSet<int> list)
+        {
+            foreach(var elem in list)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
