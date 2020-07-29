a38471fbe7ba0896e1eaaa4cc7ca50301cbc666f
AllenNotAlan <allenloyola23@gmail.com>
2020-07-29 20:42:36 +0100

    set solution added

---
+++ Sets_14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testList = new List<int> { 1, 1, 2, 2, 3, 4, 5, 5, 6 };
+
+            Program test = new Program();
+
+            var result = test.removeDuplicates(testList);
+            printSet(result);
+
+        }
+
+        public HashSet<int> removeDuplicates (List<int> list)
+        {
+            HashSet<int> result = new HashSet<int>();
+
+            foreach (var elem in list)
+            {
+                result.Add(elem);
+            }
+
+            return result;
+
+        }
+
+        public static void printSet(HashSet<int> set)
+        {
+            foreach(var elem in set)
+            {
+                Console.WriteLine(elem);
+            }
+        }
+    }
+}
