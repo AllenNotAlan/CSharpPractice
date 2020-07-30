879b4b77a4e442c4bfad28a9b41d8f09b81c53a6
AllenNotAlan <allenloyola23@gmail.com>
2020-07-30 22:43:34 +0100

    reverse a string sentence solution added

---
+++ 15_ReverseWordOrder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _15_ReverseWordOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a sentence to be reversed:");
+            string s = Console.ReadLine();
+
+            Program test = new Program();
+            Console.WriteLine(test.reverseSentence(s));
+
+        }
+
+        public string reverseSentence(string s)
+        {
+
+            string pattern = " ";
+            string input = s;
+
+            string[] StringArray = Regex.Split(input, pattern);
+            StringBuilder builder = new StringBuilder();
+            
+            for (int i = StringArray.Length - 1; i >= 0; i--)
+            {
+                builder.Append(StringArray[i]);
+                builder.Append(" ");
+            }
+
+            return builder.ToString();
+
+        }
+
+    }
+}
