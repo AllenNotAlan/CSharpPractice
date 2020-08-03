a150e23f852ad03fef06179d4997a2873d58c1ce
AllenNotAlan <allenloyola23@gmail.com>
2020-08-03 23:44:23 +0100

    elem search added

---
+++ 20_ElementSearch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _20_ElementSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> li = new List<int> { 1, 5, 3, 4, 5, 13, 7, 8, 12 };
+
+            Program test = new Program();
+
+            Console.WriteLine(test.elemSearch(li, 6));
+
+        }
+
+        public bool elemSearch(List<int> list, int num)
+        {
+            list.Sort();
+
+            bool result = false;
+            foreach(var elem in list)
+            {
+                if(elem == num)
+                {
+                    result = true;
+                }
+            }
+
+
+
+            return result;
+        }
+    }
+}
